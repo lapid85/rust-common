b3afb7d19023b934384f5ba8a2cf23ca050c7030
bard <bard@gmail.com>
2023-11-10 21:34:31 +0800

    修改文件

--- src/consts/rd.rs
+++ src/consts/rd.rs
@@ -6,3 +6,9 @@ pub const RD_PORT: u32 = 6379;
 
 /// Redis Auth
 pub const RD_AUTH: &'static str = "";
+
+/// 用户redis前缀
+pub const USER_REDIS_PREFIX: &'static str = "user:id:";
+
+/// 用户redis前缀
+pub const USER_REDIS_USER_PREFIX: &'static str = "user:name:";
